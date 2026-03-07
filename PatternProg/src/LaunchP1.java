726aecba30b00ddd49a14509ccbfe5d587a199d7
Zayyni <zainulislam.733@gmail.com>
2026-03-08 01:38:02 +0500

    Implemented some patterns

---
+++ PatternProg/src/LaunchP1.java
@@ -0,0 +1,13 @@
+public class LaunchP1 {
+
+
+    public static void main(String[] args) {
+        int n=10;
+        for (int i = 1; i <n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
