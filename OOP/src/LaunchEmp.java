606744c66bea71fab3720477ebab41d19ed199e3
Zayyni <zainulislam.733@gmail.com>
2026-04-05 03:07:30 +0500

    oop encap flow

---
+++ OOP/src/LaunchEmp.java
@@ -0,0 +1,32 @@
+class Employe {
+    private int id;
+    private String name;
+
+    void setId(int i){
+        id=i;
+    }
+
+    void setName(String n){
+        name=n;
+    }
+
+    int getId(){
+        return id;
+    }
+
+    String getName(){
+        return name;
+    }
+
+}
+public class LaunchEmp {
+    public static void main(String[] args) {
+
+        Employe emp = new Employe();
+        emp.setId(1);
+        emp.setName("Zayyni");
+        System.out.println(emp.getId());
+        System.out.println(emp.getName());
+
+    }
+}
--- OOP/src/LaunchEncap.java
+++ OOP/src/LaunchEncap.java
@@ -1,12 +1,17 @@
 class Book{
     private int pageNum;
 
+    //setter
     void setPageNum(int pageNum){
         if(pageNum < 0)
                     System.out.println("Invalid Page Number");
         else {
             this.pageNum = pageNum;
         }
+    }
+    //getter
+    int getPageNum(){
+        return pageNum;
     }
 
     void display(){
@@ -17,6 +22,7 @@ public class LaunchEncap {
     public static void main(String[] args) {
         Book book= new Book();
         book.setPageNum(3);
+        System.out.println(book.getPageNum());
         book.display();
 
     }
