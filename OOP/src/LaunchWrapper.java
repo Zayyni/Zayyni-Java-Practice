b52830c3851647140dd673fbce52ad4837c935fb
Zayyni <zainulislam.733@gmail.com>
2026-04-08 01:40:48 +0500

    using wrapper classes

---
+++ OOP/src/LaunchWrapper.java
@@ -0,0 +1,20 @@
+public class LaunchWrapper {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(a);
+
+        Integer i = Integer.valueOf(110);
+        System.out.println(" " + i);
+
+        //Boxing Converting primitive data type into its corresponding wrapper class object
+        //Auto Boxing : Automatically converting primitive data type into its corresponding wrapper class object
+        int n1=18;
+        Integer n2=n1;
+
+        //UnBoxing : Converting wrapper class object into its corresponding primitive date type
+        //Auto UnBoxing : Automatically Converting wrapper class object into its corresponding primitive date type
+
+        Integer n3 = Integer.valueOf(11);
+        int n4=n3;
+    }
+}
