7969f6bcd653dce3501d23efd948dd4004f71760
Zayyni <zainulislam.733@gmail.com>
2026-04-08 01:23:45 +0500

    using ternary opertion

---
+++ OOP/src/LaunchTernary.java
@@ -0,0 +1,9 @@
+public class LaunchTernary {
+    public static void main(String[] args) {
+        int num1=10;
+        int num2=3;
+        int result= (num1>num2) ? num1:num2;
+        System.out.println("Greater num is "+ result);
+
+    }
+}
