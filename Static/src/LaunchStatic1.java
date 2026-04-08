d2b51d535ad6fe19f4a5a132b5b5565ad3dd368e
Zayyni <zainulislam.733@gmail.com>
2026-04-09 01:07:03 +0500

    static method in main class will execute first then the main method

--- Static/src/LaunchStatic1.java
+++ Static/src/LaunchStatic1.java
@@ -1,25 +1,34 @@
-class Demo{
-    static int a,b,c;
-
-//    int x, y, z;
-
-    static {
-        System.out.println("static block");
-        a=10;
-        b=20;
-        c=30;
-    }
-
-    static void display(){
-        System.out.println("a : " + a);
-        System.out.println("b : " + b);
-        System.out.println("c : " + c);
-
-    }
-}
+//class Demo{
+//    static int a,b,c;
+//
+////    int x, y, z;
+//
+//    static {
+//        System.out.println("static block");
+//        a=10;
+//        b=20;
+//        c=30;
+//    }
+//
+//    static void display(){
+//        System.out.println("a : " + a);
+//        System.out.println("b : " + b);
+//        System.out.println("c : " + c);
+//
+//    }
+//}
 
 public class LaunchStatic1 {
+
+    static int a;
+    static {
+        a=10;
+        System.out.println("a : "+a);
+        System.out.println("static block");
+
+    }
     public static void main(String[] args) {
-        Demo.display();
+//        Demo.display();
+        System.out.println(a);
     }
 }
