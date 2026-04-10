a54f964511edfa9dffcde09b1d86c38a64d2f514
Zayyni <zainulislam.733@gmail.com>
2026-04-11 01:12:47 +0500

    Multiple Inheritance is not allowed in java

--- Inheritence/src/LaunchInheritance2.java
+++ Inheritence/src/LaunchInheritance2.java
@@ -11,23 +11,23 @@ class CargoPlane extends AeroPlane{
 
 }
 
-class CommercialPlane extends CargoPlane{
+//class CommercialPlane extends CargoPlane , AeroPlane{ A class can not extend multiple classes
+//}
+//Mutiplelevel inheritance is not allowed in java
+class PassengerPlane extends AeroPlane{
 
 }
-//class PassengerPlane extends AeroPlane{
-//
-//}
 public class LaunchInheritance2 {
     public static void main(String[] args) {
         CargoPlane cp = new CargoPlane();
         cp.fly();
         cp.landing();
         cp.getClass();
-//        PassengerPlane pp = new PassengerPlane();
-//        pp.fly();
-//        pp.landing();
-        CommercialPlane cp2 = new CommercialPlane();
-        cp2.fly();
-        cp2.landing();
+        PassengerPlane pp = new PassengerPlane();
+        pp.fly();
+        pp.landing();
+//        CommercialPlane cp2 = new CommercialPlane();
+//        cp2.fly();
+//        cp2.landing();
     }
 }
