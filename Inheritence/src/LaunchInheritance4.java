1275702a78d9fd74cc2e8b360c8b36e904401ce3
Zayyni <zainulislam.733@gmail.com>
2026-04-11 01:36:09 +0500

    Constructor do not participate in inheritance

--- Inheritence/src/LaunchInheritance4.java
+++ Inheritence/src/LaunchInheritance4.java
@@ -1,16 +1,16 @@
-class Parent
-{
-    void display(){
-        System.out.println("Parent Class");
-    }
-
-}
-class Child extends Parent{
-
-}
+//class Parent extends Child
+//{
+//    void display(){
+//        System.out.println("Parent Class");
+//    }
+//
+//}
+//class Child extends Parent{
+//
+//}
 public class LaunchInheritance4 {
     public static void main(String[] args) {
-        Child c = new Child();
-        c.display();
+//        Child c = new Child();
+//        c.display();
     }
 }
---
+++ Inheritence/src/LaunchInheritance5.java
@@ -0,0 +1,15 @@
+class Zayyni2{
+
+    Zayyni2(){
+        System.out.println("Zayyni2 zero param constructor");
+    }
+}
+class Alien2 extends Zayyni2{
+
+}
+public class LaunchInheritance5 {
+    public static void main(String[] args) {
+        Alien2 al = new Alien2();
+
+    }
+}
