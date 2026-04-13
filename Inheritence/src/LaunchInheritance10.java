8605ee5fe4263c58393de0a2c75cee817491b26a
Zayyni <zainulislam.733@gmail.com>
2026-04-13 23:51:52 +0500

    we can increase the visibility while overriding the inherited method

--- Inheritence/src/LaunchInheritance10.java
+++ Inheritence/src/LaunchInheritance10.java
@@ -2,12 +2,25 @@ class Animal{
    public void eat(){
        System.out.println("Animals are required to eat");
    }
+
+   void sleep(){
+       System.out.println("Animals are required to sleep");
+   }
 }
 class Tiger extends Animal{
 
-    void eat(){
+//    void eat(){ //cannot reduce the visibility while overriding inherited methods
+//        System.out.println("Tigers are required to eat");
+//    }
+
+    public void eat(){
         System.out.println("Tigers are required to eat");
     }
+
+    protected void sleep(){ //'we can increase the visibility while overriding the inherited method
+        System.out.println("Tigers are required to sleep");
+    }
+
 }
 public class LaunchInheritance10 {
     public static void main(String[] args) {
