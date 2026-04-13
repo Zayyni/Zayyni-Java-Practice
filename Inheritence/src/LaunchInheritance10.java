bb9a2b9bf6de149a5ba367b71f52d492e15f6d11
Zayyni <zainulislam.733@gmail.com>
2026-04-13 23:46:01 +0500

    inherit behaviour of animal for the tiger

--- Inheritence/src/LaunchInheritance10.java
+++ Inheritence/src/LaunchInheritance10.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+class Animal{
+   public void eat(){
+       System.out.println("Animals are required to eat");
+   }
+}
+class Tiger extends Animal{
 
+}
 public class LaunchInheritance10 {
+    public static void main(String[] args) {
+        Tiger t = new Tiger();
+        t.eat();
+    }
 }
