062bcfaded3894f1696d4173bc13bc61c6811165
Zayyni <zainulislam.733@gmail.com>
2026-04-14 23:43:02 +0500

    final methods do participate in inheritance

--- Inheritence/src/LaunchInheritanceFinal.java
+++ Inheritence/src/LaunchInheritanceFinal.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+//final class Plane1{ //final class cannot be inherited or cannot be Parent of any child class
+//
+//    public void fly(){
+//        System.out.println("Planes are required to fly");
+//    }
+//}
 
+class Plane1{ //final class cannot be inherited or cannot be Parent of any child class
+
+    final void fly(){
+        System.out.println("Planes are required to fly");
+    }
+}
+class FighterPlane1 extends Plane1{
+
+//    public void fly(){
+//        System.out.println("FighterPlane 1 is flying");
+//    }
+
+}
 public class LaunchInheritanceFinal {
+    public static void main(String[] args) {
+        FighterPlane1 plane1 = new FighterPlane1();
+        plane1.fly();
+    }
 }
