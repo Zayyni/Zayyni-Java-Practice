2c4030064f8ec5e0296671df116b82ed8176dcf5
Zayyni <zainulislam.733@gmail.com>
2026-04-14 23:47:53 +0500

    final field cannot be reassigned

--- Inheritence/src/LaunchInheritanceFinal.java
+++ Inheritence/src/LaunchInheritanceFinal.java
@@ -7,11 +7,15 @@
 
 class Plane1{ //final class cannot be inherited or cannot be Parent of any child class
 
+    final int age =10;
+
     final void fly(){ // final methods do participate in inheritance
+//        age=20;
         System.out.println("Planes are required to fly");
     }
 }
 class FighterPlane1 extends Plane1{
+
 
 //    public void fly(){ //cannot override the final method in child class
 //        System.out.println("FighterPlane 1 is flying");
