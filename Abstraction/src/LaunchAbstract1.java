c850af64abfa893bd026f0f2a63c88ffeab0daf1
Zayyni <zainulislam.733@gmail.com>
2026-04-17 21:19:57 +0500

    to implement a abstract method it is mandatory to have the class type as abstract

--- Abstraction/src/LaunchAbstract1.java
+++ Abstraction/src/LaunchAbstract1.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
+abstract class Animal {
+    public abstract void eat();
+
+    public abstract void run();
+}
+class Tiger extends Animal {
+    public void eat(){
+        System.out.println("Tiger hunts and eats");
+    }
+
+    public void run(){
+        System.out.println("Tiger is Running to hunt");
+    }
+}
+class Monkey extends Animal {
+    public void eat(){
+        System.out.println("Monkey steals and eats...");
+    }
+
+    public void run(){
+        System.out.println("Monkey is Running to steal");
+    }
+}
+class Forest{
+
+    public void permit(Animal animal){
+        animal.eat();
+        animal.run();
+    }
+}
 
 public class LaunchAbstract1 {
+    public static void main(String[] args) {
+
+    }
 }
