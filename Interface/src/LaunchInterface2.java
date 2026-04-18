d0ce26fd2b869151b45e75760fd2b35571475e4b
Zayyni <zainulislam.733@gmail.com>
2026-04-19 01:31:44 +0500

    an interface cannot implement an other interface but it can extend other interface and can have its own methods as well

---
+++ Interface/src/LaunchInterface2.java
@@ -0,0 +1,31 @@
+//interface Demo1{
+//    void display();
+//}
+//interface Demo2 implements Demo1{ //it is illegal for an interface to implement another interface
+//
+//}
+
+interface Demo1{
+    void display();
+}
+interface Demo2 extends Demo1{
+    void show();
+}
+class Alpha implements Demo2{
+    public void display(){
+        System.out.println("Display of Alpha");
+    }
+    public void show(){
+        System.out.println("Show of Alpha");
+    }
+}
+public class LaunchInterface2 {
+    public static void main(String[] args) {
+       Demo1 demo1= new Alpha();
+       demo1.display();
+
+        Demo2 demo2= new Alpha();
+        demo2.display();
+        demo2.show();
+    }
+}
