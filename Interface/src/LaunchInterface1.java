ce58aefff0c64366cef8334ed54a0b7e2844db58
Zayyni <zainulislam.733@gmail.com>
2026-04-19 00:41:02 +0500

    all the methods inside the interface class are abstract

---
+++ Interface/src/LaunchInterface1.java
@@ -0,0 +1,10 @@
+interface Calc
+{
+    void add(int a, int b);//public abstract void add(int a, int b);
+}
+
+public class LaunchInterface1 {
+    public static void main(String[] args) {
+
+    }
+}
