a0992a3b73e0b561b8454f7fc3f9fd153d68a065
Zayyni <zainulislam.733@gmail.com>
2026-04-20 00:33:40 +0500

    implemented loose coupling using the strategy pattern

--- AmazonAssociationProject1/src/main/Amazon.java
+++ AmazonAssociationProject1/src/main/Amazon.java
@@ -1,13 +1,21 @@
 package main;
 
 import services.FedEx;
+import services.IDeliveryService;
 
 public class Amazon
 {
+
+    private IDeliveryService service;
+
+    public void setService(IDeliveryService service) //IDeliveryService service = new FedEx();
+    {
+        this.service = service;
+    }
     public Boolean deliverProduct(Double amount)
     {
-        FedEx fedEx = new FedEx();
-        return fedEx.deliverProduct(amount);
+//        FedEx fedEx = new FedEx();
+        return service.deliverProduct(amount);
 
     }
 }
--- AmazonAssociationProject1/src/main/LaunchAmazon.java
+++ AmazonAssociationProject1/src/main/LaunchAmazon.java
@@ -1,7 +1,23 @@
 package main;
 
+import services.FedEx;
+
 public class LaunchAmazon {
     public static void main(String[] args) {
 
+        Amazon amazon = new Amazon();
+//        FedEx fedEx = new FedEx();
+//        amazon.setService(fedEx);
+        amazon.setService(new FedEx());
+
+
+
+
+        Boolean status = amazon.deliverProduct(200.9);
+        if (status){
+            System.out.println("Product delivered Successfully");
+        }else {
+            System.out.println("Product not delivered Successfully");
+        }
     }
 }
