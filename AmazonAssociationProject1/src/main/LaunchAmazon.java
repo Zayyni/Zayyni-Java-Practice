110210adc431dab5afd944539ef165755c7487b5
Zayyni <zainulislam.733@gmail.com>
2026-04-20 01:09:01 +0500

    given the description of the classes which are being used in the project

--- AmazonAssociationProject1/src/main/LaunchAmazon.java
+++ AmazonAssociationProject1/src/main/LaunchAmazon.java
@@ -9,6 +9,17 @@ public class LaunchAmazon {
 
         //constructor Injection
         Amazon amazon = new Amazon(new FirstFlight());
+        //Injecting dependent object/dependency into target class is called dependency injection
+        //If this is done through setter we call this as Setter injection
+        //If this is done through constructor we call this as Constructor Injection
+
+        // target class/Object ==> a class where service of other classes are being used
+        //eg in this project : Amazon Class
+
+        //dependent class/object ==> a class whose service is being used by target class
+        // eg in this project : FedEx Class, BlueDart Class, FirstFlight Class
+
+
 //        FedEx fedEx = new FedEx();
 //        amazon.setService(fedEx);
         //setter Injection
