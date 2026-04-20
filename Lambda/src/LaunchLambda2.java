b01f8b27a22a5ba0215d3d1a1e2dec05c91fc911
Zayyni <zainulislam.733@gmail.com>
2026-04-21 01:55:24 +0500

    we can write lambda functions in single line as well

--- Lambda/src/LaunchLambda2.java
+++ Lambda/src/LaunchLambda2.java
@@ -2,6 +2,10 @@ interface Animal1 {
     void eat();
 }
 
+
+interface Animal2 {
+    void getData();
+}
 public class LaunchLambda2 {
     public static void main(String[] args) {
 
@@ -11,7 +15,14 @@ public class LaunchLambda2 {
                 System.out.println("eat method in interface");
             }
         };
+//        Animal1 a = () -> System.out.println("in side the animal interface");
         animal.eat();
+
+        Animal2 animal2 = () ->{
+                System.out.println("getData method in interface");
+        };
+
+        animal2.getData();
 
     }
 }
