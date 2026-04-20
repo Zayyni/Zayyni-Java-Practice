f6b6876d73c493993d38f886543384b9225bbde9
Zayyni <zainulislam.733@gmail.com>
2026-04-21 00:57:27 +0500

    initial commit for lambda implementation

---
+++ Lambda/src/Demo.java
@@ -0,0 +1,46 @@
+//class A{
+//    public void show(){
+//        System.out.println("in B Show method");
+//    }
+//}
+//
+//class B extends A{
+//    public void show(){
+//        System.out.println("in B Show method");
+//    }
+//}
+
+class Person{
+
+//    int age;
+//    String name;
+    public void printName(){
+        System.out.println("Zayyni");
+    }
+
+    class Phone{
+//        int price;
+//        String model;
+
+        public void details()
+        {
+            System.out.println("SamSung");
+        }
+    }
+
+}
+
+public class Demo {
+    public static void main(String[] args) {
+
+//        A a = new A();
+//        A a = new B();
+//        a.show();
+
+        Person p = new Person();
+        p.printName();
+//        p.age = 18;
+//        p.name = "John Smith";
+
+    }
+}
