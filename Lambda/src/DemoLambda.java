ec6dfda08498dbd822fb8f6a7777f678b0db50b7
Zayyni <zainulislam.733@gmail.com>
2026-04-21 01:30:00 +0500

    implemented anonymous inner class for a class object

--- Lambda/src/DemoLambda.java
+++ Lambda/src/DemoLambda.java
@@ -1,51 +1,55 @@
-//class A{
-//    public void show(){
-//        System.out.println("in B Show method");
-//    }
-//}
+//class Person{
 //
-//class B extends A{
-//    public void show(){
-//        System.out.println("in B Show method");
+////    int age;
+////    String name;
+//    public void printName(){
+//        System.out.println("Zayyni");
 //    }
+//
+//     class Phone{
+    ////        int price;
+    ////        String model;
+//
+//        public void details()
+//        {
+//            System.out.println("SamSung");
+//        }
+//    }
+//
 //}
-/**
- * This is multiline comment for generating java doc
- *
- */
-class Person{
 
-//    int age;
-//    String name;
-    public void printName(){
-        System.out.println("Zayyni");
+class A{
+    public void show(){
+        System.out.println("in B Show method");
     }
-
-     class Phone{
-//        int price;
-//        String model;
-
-        public void details()
-        {
-            System.out.println("SamSung");
-        }
-    }
-
 }
+
+class B extends A{
+    public void show(){
+        System.out.println("in B Show method");
+    }
+}
+
+
 
 public class DemoLambda {
     public static void main(String[] args) {
 
-//        A a = new A();
+        A a = new A() //Anonymous Inner class
+        {
+            public void show(){
+                System.out.println("in New Show method");
+            }
+        };
 //        A a = new B();
-//        a.show();
+        a.show();
 
-        Person p = new Person();
-        p.printName();
+//        Person p = new Person();
+//        p.printName();
 //        p.age = 18;
 //        p.name = "John Smith";
-        Person.Phone phone = p.new Phone();
-        phone.details();
+//        Person.Phone phone = p.new Phone();
+//        phone.details();
 
     }
 }
