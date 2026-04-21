f2ed3c71ee91e9e6fd93d32f90aef10481e465ce
Zayyni <zainulislam.733@gmail.com>
2026-04-22 01:36:44 +0500

    implemented multiple exception handling cases

---
+++ ExceptionHandling/src/LaunchExcept2.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class LaunchExcept2 {
+    public static void main(String[] args) {
+
+        System.out.println("Connection Established");
+        try {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Kindly Enter the Numerator for division : ");
+            int numerator = scan.nextInt();
+            System.out.println("Kindly Enter the Denominator for division : ");
+            int denominator = scan.nextInt();
+
+            int result = numerator / denominator;
+            System.out.println("Result is : " + result);
+
+            System.out.println("Kindly Enter the size of array : ");
+            int size = scan.nextInt();
+
+            int[] array = new int[size];
+
+            System.out.println("Kindly Enter the elements to be inserted into array : ");
+            int elements = scan.nextInt();
+
+            System.out.println("Kindly Enter the position at which element to be inserted into array : ");
+            int position = scan.nextInt();
+            array[position] = elements;
+
+            System.out.println("Data inserted into array : " + array[position]);
+        }
+        catch (ArithmeticException e) {
+            System.out.println("Please enter a positive integer");
+        }
+        catch (ArrayIndexOutOfBoundsException e)
+        {
+            System.out.println("Array index out of bounds");
+        }
+        catch (NegativeArraySizeException e)
+        {
+            System.out.println("Negative array size");
+        }
+        catch (Exception e) {
+            System.out.println("Something went wrong");
+        }
+
+        System.out.println("Connection Terminated");
+
+    }
+}
