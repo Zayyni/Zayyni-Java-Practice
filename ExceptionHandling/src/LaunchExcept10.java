757818d88c997e4ff70865e67c4ac6ebb64bdc19
Zayyni <zainulislam.733@gmail.com>
2026-04-23 01:30:26 +0500

    if the parent class method throws checked exception then the child class methods can also throw both type of exception like checked or unchecked but the cannot throw partially throwing exception

--- ExceptionHandling/src/LaunchExcept10.java
+++ ExceptionHandling/src/LaunchExcept10.java
@@ -17,8 +17,30 @@ import java.io.IOException;
 //}
 
 
+//class Parent{
+//    public void work() throws ArithmeticException{
+//        System.out.println("Parent work");
+//    }
+//}
+//class Child1 extends Parent{
+//    public void work() throws ArithmeticException{
+//        System.out.println("Child1 work");
+//    }
+//}
+//class Child2 extends Parent{
+//    public void work() {
+//        System.out.println("Child2 work");
+//    }
+//}
+//
+//class Child3 extends Parent{
+//    public void work() throws IOException {
+//        System.out.println("Child3 work");
+//    }
+//}
+
 class Parent{
-    public void work() throws ArithmeticException{
+    public void work() throws IOException{
         System.out.println("Parent work");
     }
 }
@@ -38,6 +60,12 @@ class Child3 extends Parent{
         System.out.println("Child3 work");
     }
 }
+class Child4 extends Parent{
+    public void work() throws Exception {
+        System.out.println("Child4 work");
+    }
+}
+
 public class LaunchExcept10 {
     public static void main(String[] args) {
 
