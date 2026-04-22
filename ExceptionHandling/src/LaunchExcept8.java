5116ef67bfe73ca1720f32b85ced783ceff85de6
Zayyni <zainulislam.733@gmail.com>
2026-04-23 01:06:38 +0500

    here in this class we are going to get stackover flow exception

---
+++ ExceptionHandling/src/LaunchExcept8.java
@@ -0,0 +1,15 @@
+class Alien{
+    public void alpha(){
+
+    };
+    public void beta(){
+
+    }
+}
+
+public class LaunchExcept8 {
+    public static void main(String[] args) {
+        Alien alien = new Alien();
+        alien.beta();
+    }
+}
