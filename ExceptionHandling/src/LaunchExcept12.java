19d65b9c43eec949adda27b199b5d0d86135921c
Zayyni <zainulislam.733@gmail.com>
2026-04-24 02:26:18 +0500

    a method can throw multiple exceptions at a time using or operator

---
+++ ExceptionHandling/src/LaunchExcept12.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+class UnderAgeException extends Exception {
+    public UnderAgeException(String message) {
+         super(message);
+    }
+
+}
+class OverAgeException extends Exception {
+    public OverAgeException(String message) {
+        super(message);
+    }
+
+}
+
+class Applicant{
+    int age;
+    public void input(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter age: ");
+        age = sc.nextInt();
+    }
+
+    public void verify() throws UnderAgeException, OverAgeException{
+        if(age >= 18 && age <= 60){
+            System.out.println("You can proceed with Licence Application");
+        }
+        else if(age < 18){
+           UnderAgeException underAgeException = new UnderAgeException("Under age");
+           System.out.println(underAgeException.getMessage());
+           throw underAgeException;
+        }
+        else{
+            OverAgeException overAgeException = new OverAgeException("Over age");
+            System.out.println(overAgeException.getMessage());
+            throw overAgeException;
+        }
+    }
+}
+class Licence{
+    public void application(){
+        Applicant applicant = new Applicant();
+        try {
+            applicant.input();
+            applicant.verify();
+        }
+        catch (UnderAgeException | OverAgeException e){
+            System.out.println("Try again!");
+            try {
+                applicant.input();
+                applicant.verify();
+            }
+            catch (UnderAgeException | OverAgeException e1){
+                System.out.println("You are blocked to proceed with Licence Application");
+            }
+        }
+    }
+}
+public class LaunchExcept12 {
+    public static void main(String[] args) {
+
+    }
+}
