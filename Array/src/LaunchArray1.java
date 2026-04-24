f2821a3b3d5e532be019d6aaf066f8af4c531db7
Zayyni <zainulislam.733@gmail.com>
2026-04-25 00:53:26 +0500

    initial commit for array understanding

---
+++ Array/src/LaunchArray1.java
@@ -0,0 +1,13 @@
+public class LaunchArray1 {
+    public static void main(String[] args) {
+
+        int []marks = new int[5];
+        marks[0] = 10;
+        marks[1] = 20;
+        marks[2] = 30;
+        marks[3] = 40;
+        marks[4] = 50;
+
+        System.out.println(marks[0]);
+    }
+}
