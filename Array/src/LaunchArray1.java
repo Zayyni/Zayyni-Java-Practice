5cc27fee1c32f4e3da9f001cd78c82d9b30908ef
Zayyni <zainulislam.733@gmail.com>
2026-04-25 01:07:51 +0500

    used for loops in the arrays to create a students marks index program

--- Array/src/LaunchArray1.java
+++ Array/src/LaunchArray1.java
@@ -1,13 +1,27 @@
+import java.util.Scanner;
+
 public class LaunchArray1 {
     public static void main(String[] args) {
 
         int []marks = new int[5];
-        marks[0] = 10;
-        marks[1] = 20;
-        marks[2] = 30;
-        marks[3] = 40;
-        marks[4] = 50;
+//        marks[0] = 10;
+//        marks[1] = 20;
+//        marks[2] = 30;
+//        marks[3] = 40;
+//        marks[4] = 50;
+//
+//        System.out.println(marks[0]);
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < marks.length; i++) {
+            System.out.println("Kindly enter the marks of the student " + i);
+            marks[i] = sc.nextInt();
+        }
+        System.out.println("The marks of the students are: ");
+        for (int i = 0; i < marks.length; i++) {
+            System.out.print(marks[i] + " ");
+        }
+        System.out.println();
 
-        System.out.println(marks[0]);
+
     }
 }
