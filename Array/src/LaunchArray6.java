1b853408aa29d1c867289b0e30fd57a97642b484
Zayyni <zainulislam.733@gmail.com>
2026-04-26 00:48:13 +0500

    used in built Arrays util class to use its static methods as we cannot create instance of the arrays class we can directly use these methods

--- Array/src/LaunchArray6.java
+++ Array/src/LaunchArray6.java
@@ -1,7 +1,36 @@
+import java.util.Arrays;
+
+class Zayyni{
+     String courseName;
+     int courseCost;
+     String city;
+}
 public class LaunchArray6
 {
     public static void main(String[] args)
     {
+        Zayyni obj = new Zayyni();
+        System.out.println(obj.getClass().getName());
+
+        System.out.println("**************************************");
+//        TShaped t2 = new TShaped();
+
+        int a[] = new int[3];
+        System.out.println(a);
+        System.out.println(a.getClass().getName());
+
+        System.out.println("**************************************");
+
+        int ar[][] = new int[3][5];
+        System.out.println(ar);
+        System.out.println(ar.getClass().getName());
+
+        System.out.println("**************************************");
+
+        int array[] = {3,5,3,7,8};
+        Arrays.sort(array);
+        System.out.println(array);
+
 
     }
 }
