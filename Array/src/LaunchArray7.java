fb6eb0b5fbaf0952512ff92931ef6d19dce490d9
Zayyni <zainulislam.733@gmail.com>
2026-04-26 01:57:13 +0500

    the enhanced for loop is best for just traversing the list or array as we cannot perfrom any custom operations on the array

--- Array/src/LaunchArray7.java
+++ Array/src/LaunchArray7.java
@@ -2,7 +2,7 @@ public class LaunchArray7
 {
     public static void main(String[] args)
     {
-        int a[] = {3,4,5,3,2};
+//        int a[] = {3,4,5,3,2};
 
 //        for (int i = 0; i< a.length; i++){
 //            System.out.println(a[i]);
@@ -14,25 +14,30 @@ public class LaunchArray7
 //            System.out.print(elem);
 //        }
 
-        int arr[][] = {{3,4,5},{6,7,8}};
+//        int arr[][] = {{3,4,5},{6,7,8}};
+//
+//        for(int i = 0; i < arr.length; i++){
+//            for(int j = 0; j < arr[i].length; j++){
+//                System.out.print(arr[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+//
+//        System.out.println("**********************************");
+//
+//        for(int row[]:arr)
+//        {
+//            for (int elem:row){
+//                System.out.print(elem + " ");
+//            }
+//            System.out.println();
+//        }
 
-        for(int i = 0; i < arr.length; i++){
-            for(int j = 0; j < arr[i].length; j++){
-                System.out.print(arr[i][j] + " ");
-            }
-            System.out.println();
+        int a[] = {3,4,2,3,4,5};
+
+        for( int i=a.length-1; i>=0; i--){
+            System.out.print(a[i] + " ");
         }
-
-        System.out.println("**********************************");
-
-        for(int row[]:arr)
-        {
-            for (int elem:row){
-                System.out.print(elem + " ");
-            }
-            System.out.println();
-        }
-
-
+        System.out.println();
     }
 }
