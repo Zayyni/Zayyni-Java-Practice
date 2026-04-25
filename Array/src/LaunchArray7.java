d72a0e48fc578529ee03c2780b5f9128218aa8f7
Zayyni <zainulislam.733@gmail.com>
2026-04-26 01:26:54 +0500

    implemented 2d array using the advanced/enhanced for loop to use in the program

--- Array/src/LaunchArray7.java
+++ Array/src/LaunchArray7.java
@@ -3,15 +3,36 @@ public class LaunchArray7
     public static void main(String[] args)
     {
         int a[] = {3,4,5,3,2};
-        for (int i = 0; i< a.length; i++){
-            System.out.println(a[i]);
+
+//        for (int i = 0; i< a.length; i++){
+//            System.out.println(a[i]);
+//        }
+//
+//        System.out.println("**************************************");
+//
+//        for(int elem: a){
+//            System.out.print(elem);
+//        }
+
+        int arr[][] = {{3,4,5},{6,7,8}};
+
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[i].length; j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
         }
 
-        System.out.println("**************************************");
+        System.out.println("**********************************");
 
-        for(int elem: a){
-            System.out.print(elem);
+        for(int row[]:arr)
+        {
+            for (int elem:row){
+                System.out.print(elem + " ");
+            }
+            System.out.println();
         }
+
 
     }
 }
