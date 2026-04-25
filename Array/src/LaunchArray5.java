51ce0643875a2982c79150c0812ed8b819c51361
Zayyni <zainulislam.733@gmail.com>
2026-04-26 00:34:55 +0500

    declaring the array if the array elements are already known

---
+++ Array/src/LaunchArray5.java
@@ -0,0 +1,26 @@
+public class LaunchArray5 {
+    public static void main(String[] args) {
+
+//        int arr[];
+//        arr = new int[5];
+//        arr[0] = 1;
+//        arr[1] = 2;
+//        arr[2] = 3;
+//        arr[3] = 4;
+//        arr[4] = 5;
+//        arr[5] = 6;//Array out of Index Bound Exception Runtime
+
+        int arr [] = new int[]{2,3,4};
+
+        int arr1[] = {1,3,5,8};
+        System.out.println(arr[0]);
+
+        int arr2[][] = {{2,5,6,8},{5,45,6,9}};
+        System.out.println(arr2[1][1]);
+
+        int arr3[][] ={{2,6,8,8},{5,6,9}};
+        System.out.println(arr3[1][1]);
+
+        
+    }
+}
