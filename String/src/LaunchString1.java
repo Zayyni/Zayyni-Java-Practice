16e1bac242bbb71e09368a80ec3e9dc8d23b4a26
Zayyni <zainulislam.733@gmail.com>
2026-04-27 01:08:55 +0500

    in heap we have string constant pool which does not allows the duplicate but where as the heap memory allows the duplicates so here the equals compares the string data and == compare the string reference

---
+++ String/src/LaunchString1.java
@@ -0,0 +1,20 @@
+public class LaunchString1
+{
+    public static void main(String[] args) {
+//        String str1 = "Zayyni";
+//        String str2 = "Zayyni";
+//        System.out.println(str1 == str2); //true
+
+//        String str1 = new String("Zayyni");
+//        String str2 = new String("Zayyni");
+//        System.out.println(str1 == str2); //false
+//        System.out.println(str1.equals(str2)); //true
+
+        String str1 = "Zayyni";
+        String str2 = "zayyni";
+        System.out.println(str1 == str2); //false
+        System.out.println(str1.equals(str2)); //false
+
+
+    }
+}
