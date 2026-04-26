1fa1a26d72a3763dd9e507908b323fcbdb4c660c
Zayyni <zainulislam.733@gmail.com>
2026-04-27 01:46:30 +0500

    when we create a string concate object then the java reference variable referes to new string on concate to store that concate value it does not change the original value that is why string are immutable

---
+++ String/src/LaunchString2.java
@@ -0,0 +1,23 @@
+public class LaunchString2 {
+    public static void main(String[] args) {
+
+//        String str1 = "Zayyni";
+//
+//        System.out.println(str1);
+//        str1.concat(" Dev");
+//        System.out.println(str1); // data will remain same as the str1 references to only first string object
+
+//        String str1 = "Zayyni";
+//
+//        System.out.println(str1);
+//        str1=str1.concat(" Dev");
+//        System.out.println(str1);
+
+        String str1 = "Zayyni";
+
+        System.out.println(str1);
+        String str2=str1.concat(" Dev");
+        System.out.println(str1);
+        System.out.println(str2);
+    }
+}
