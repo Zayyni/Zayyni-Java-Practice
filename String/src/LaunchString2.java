1cadc100174216159926189a981bfbed7ced08e3
Zayyni <zainulislam.733@gmail.com>
2026-04-27 01:56:55 +0500

    if we use + operator to concatinate with out reference variable then it maps to string constant pool and if there is reference vairable then it maps to heap area

--- String/src/LaunchString2.java
+++ String/src/LaunchString2.java
@@ -20,15 +20,18 @@ public class LaunchString2 {
 //        System.out.println(str1);
 //        System.out.println(str2);
 
-        String str1 = "Zayyni";
-        String str2 = str1.concat(" Dev");
-        String str3 = "Zayyni Dev";  //reference to SCP
-        System.out.println(str2==str3); //false
-
-        String str4 = str1+str2; //reference to Heap memory area
-        System.out.println(str4==str3); //false
+//        String str1 = "Zayyni";
+//        String str2 = str1.concat(" Dev");
+//        String str3 = "Zayyni Dev";  //reference to SCP
+//        System.out.println(str2==str3); //false
+//
+//        String str4 = str1+str2; //reference to Heap memory area
+//        System.out.println(str4==str3); //false
 //        System.out.println(str1);
 //        System.out.println(str2);
 //        System.out.println(str3);
+
+        String str1 = "Zayyni" + "Java" + "SpringBoot" + "MicroServices";
+        System.out.println(str1);
     }
 }
