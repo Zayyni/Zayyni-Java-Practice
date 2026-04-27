fc338e60f24b232234bbda8e34374735057e8ee5
Zayyni <zainulislam.733@gmail.com>
2026-04-28 01:33:09 +0500

    default capacity is 16 if we pass in the constrcutor then the value will be 16 + string lenght in the constructor

--- String/src/LaunchMutableString2.java
+++ String/src/LaunchMutableString2.java
@@ -1,6 +1,6 @@
 public class LaunchMutableString2 {
     public static void main(String[] args) {
-//        StringBuffer sb1 = new StringBuffer("Zayyni");
+//        StringBuffer sb1 = new StringBuffer("Zayyni"); //16 original capacity + 6 =22
         StringBuilder sb1 = new StringBuilder();
         System.out.println(sb1.capacity());
         sb1.append("Zayyni");
---
+++ String/src/LaunchMutableString3.java
@@ -0,0 +1,6 @@
+public class LaunchMutableString3
+{
+    public static void main(String[] args) {
+
+    }
+}
