f8c9140c05041765735e0c04e7c1ded4df30958c
Zayyni <zainulislam.733@gmail.com>
2026-04-28 00:31:28 +0500

    the compareTo() method while comparing will always give the value of first non matching character

--- String/src/LaunchString3.java
+++ String/src/LaunchString3.java
@@ -3,9 +3,28 @@ public class LaunchString3 {
 //        final String s1 = "Zayyni";
 //        s1=s1 + "Dev";
 
-        final String s1 = "Zayyni";
-        String s2 = s1 + " Dev";
-        System.out.println(s2);
+//        final String s1 = "Zayyni";
+//        String s2 = s1 + " Dev";
+//        System.out.println(s2);
+
+        //CompareTo() -> compares the values of String lexiographically (ascii code)
+        String s1 = "zayyni";
+        String s2 = "zya";
+        int result = s1.compareTo(s2);
+        System.out.println(result);
+        //it always gives difference of first character
+
+        if(result == 0)
+        {
+            System.out.println("Strings are equal");
+        }
+        else
+        {
+            System.out.println("Strings are not equal");
+        }
+
+
+
 
     }
 }
