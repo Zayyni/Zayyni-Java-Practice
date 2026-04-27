ec2d8e495ddf5b8d6f3e9f7d458b972e537f01aa
Zayyni <zainulislam.733@gmail.com>
2026-04-28 00:21:58 +0500

    immutability and final are not equal things as we cannot reassign the value to final variable string but to mutable string we can reassign the value

---
+++ String/src/LaunchString3.java
@@ -0,0 +1,11 @@
+public class LaunchString3 {
+    public static void main(String[] args) {
+//        final String s1 = "Zayyni";
+//        s1=s1 + "Dev";
+
+        final String s1 = "Zayyni";
+        String s2 = s1 + " Dev";
+        System.out.println(s2);
+
+    }
+}
