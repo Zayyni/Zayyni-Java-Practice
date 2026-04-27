07f6d7847cdd8df3bc12fe8c8678ff63af863604
Zayyni <zainulislam.733@gmail.com>
2026-04-28 01:30:04 +0500

    there is the capacity in the string builder which keeps on increating by uisng the formula of oldCapacity*2+2

--- String/src/LaunchMutableString2.java
+++ String/src/LaunchMutableString2.java
@@ -1,5 +1,19 @@
 public class LaunchMutableString2 {
     public static void main(String[] args) {
+//        StringBuffer sb1 = new StringBuffer("Zayyni");
+        StringBuilder sb1 = new StringBuilder();
+        System.out.println(sb1.capacity());
+        sb1.append("Zayyni");
+        sb1.trimToSize();
+//        sb1.append("Zyaa");
+//        sb1.append("java developer");
+//        System.out.println(sb1);
+//        System.out.println(sb1.capacity()); //16*2+2 = 34
+//
+//        //35  or 32*2+2 = 70
+
+
+
 
     }
 }
