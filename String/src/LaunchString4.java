ff142f84dda495dfc3821deb04fef5674c7541bd
Zayyni <zainulislam.733@gmail.com>
2026-04-28 00:44:52 +0500

    trim method is used to remove spaces from the start of the String and from the End of the String

--- String/src/LaunchString4.java
+++ String/src/LaunchString4.java
@@ -1,14 +1,19 @@
 public class LaunchString4 {
     public static void main(String[] args) {
 
-        String s1 = "zayyni dev java";
-//        String[] str = s1.split("a");
-        String[] str = s1.split(" ");
-        System.out.println(str.length);
-        for (String str1 : str) {
-            System.out.print(str1);
-        }
-        System.out.println();
+//        String s1 = "zayyni dev java";
+////        String[] str = s1.split("a");
+//        String[] str = s1.split(" ");
+//        System.out.println(str.length);
+//        for (String str1 : str) {
+//            System.out.print(str1);
+//        }
+//        System.out.println();
+
+        String s1 = "   Zayyni Dev Java   ";
+        System.out.println(s1);
+        System.out.println(s1.length());
+        System.out.println(s1.trim());
 
     }
 }
