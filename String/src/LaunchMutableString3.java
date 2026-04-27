5ed5da38a38b0d508fd4b674262f0913637c7731
Zayyni <zainulislam.733@gmail.com>
2026-04-28 01:39:14 +0500

    default capacity is 16 if we pass in the constrcutor then the value will be 16 + string lenght in the constructor

--- String/src/LaunchMutableString3.java
+++ String/src/LaunchMutableString3.java
@@ -1,6 +1,8 @@
 public class LaunchMutableString3
 {
     public static void main(String[] args) {
-
+        StringBuilder sb1 = new StringBuilder("Zayyni");
+        StringBuilder sb2 = new StringBuilder("Zayyni");
+        System.out.println(sb1.equals(sb2));
     }
 }
