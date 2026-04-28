a35b39c588201a5f7db78389540771d1eed41349
Zayyni <zainulislam.733@gmail.com>
2026-04-29 01:45:14 +0500

    implemented some initial methods of the collection arraylist list

---
+++ Collection/src/LaunchArrayList.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class LaunchArrayList {
+    public static void main(String[] args) {
+
+        ArrayList al = new ArrayList();
+        al.add(100);
+        al.add(200);
+        al.add(44.6);
+        al.add("Zayyni");
+        al.add("Dev");
+        al.add(true);
+        System.out.println(al);
+
+        ArrayList al2 = new ArrayList();
+        al2.add(100);
+        al2.add(20);
+        al2.add(0,45);
+        al2.addAll(al);
+
+        System.out.println(al2);
+    }
+}
