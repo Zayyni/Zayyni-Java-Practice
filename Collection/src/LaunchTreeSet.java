c9d09585031b83ca835aa1258e3dc419d7aa9ac9
Zayyni <zainulislam.733@gmail.com>
2026-04-29 02:25:30 +0500

    TreeSet by default stores data in accending order by sorting it as it internally use binary tree data strcuture

---
+++ Collection/src/LaunchTreeSet.java
@@ -0,0 +1,18 @@
+import java.util.TreeSet;
+
+public class LaunchTreeSet
+{
+    public static void main(String[] args) {
+        TreeSet ts = new TreeSet();
+        ts.add(100);
+        ts.add(80);
+        ts.add(90);
+        ts.add(70);
+        ts.add(60);
+        ts.add(40);
+        ts.add(10);
+        ts.add(20);
+        ts.add(123);
+        System.out.println(ts);
+    }
+}
