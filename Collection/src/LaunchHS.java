deea7494014b0b0e1faf4ed2585320717b6d5cf5
Zayyni <zainulislam.733@gmail.com>
2026-04-30 01:44:43 +0500

    LinkedHashSet does maintain order of insertion also it does not allow duplicates

--- Collection/src/LaunchHS.java
+++ Collection/src/LaunchHS.java
@@ -1,9 +1,21 @@
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 
 public class LaunchHS
 {
     public static void main(String[] args) {
-        HashSet hs = new HashSet(); //it does not maintain order of insertion also it does not allow duplicates
+//        HashSet hs = new HashSet(); //it does not maintain order of insertion also it does not allow duplicates
+//        hs.add(100);
+//        hs.add(50);
+//        hs.add(150);
+//        hs.add(25);
+//        hs.add(75);
+//        hs.add(125);
+//        hs.add(175);
+//        System.out.println(hs);
+
+
+        LinkedHashSet hs = new LinkedHashSet(); //it does maintain order of insertion also it does not allow duplicates
         hs.add(100);
         hs.add(50);
         hs.add(150);
@@ -12,5 +24,6 @@ public class LaunchHS
         hs.add(125);
         hs.add(175);
         System.out.println(hs);
+
     }
 }
