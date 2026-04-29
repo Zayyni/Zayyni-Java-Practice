79a6997df6db617525f0cdfae6a54a22fb81707e
Zayyni <zainulislam.733@gmail.com>
2026-04-30 01:56:29 +0500

    we can access the element of collection through loops like for, advanced loops but it is not recommended

---
+++ Collection/src/LaunchAccessingData.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class LaunchAccessingData
+{
+    public static void main(String[] args) {
+        ArrayList al = new ArrayList();
+        al.add(100);
+        al.add(50);
+        al.add(150);
+        al.add(70);
+        al.add(125);
+        System.out.println(al);
+
+//        double d = (double) al.get(2);
+//        System.out.println(d);
+
+//        for (int i = 0; i < al.size(); i++)
+//        {
+//            System.out.println(al.get(i));
+//        }
+
+//        for (Object ob:al)
+//        {
+//            System.out.println(ob);
+//        }
+    }
+}
