b96f6b39a346c2bdb51bcab64de9efa1cbc2e184
Zayyni <zainulislam.733@gmail.com>
2026-04-30 01:42:47 +0500

    HashSet does not maintain order of insertion also it does not allow duplicates

---
+++ Collection/src/LaunchHS.java
@@ -0,0 +1,16 @@
+import java.util.HashSet;
+
+public class LaunchHS
+{
+    public static void main(String[] args) {
+        HashSet hs = new HashSet(); //it does not maintain order of insertion also it does not allow duplicates
+        hs.add(100);
+        hs.add(50);
+        hs.add(150);
+        hs.add(25);
+        hs.add(75);
+        hs.add(125);
+        hs.add(175);
+        System.out.println(hs);
+    }
+}
