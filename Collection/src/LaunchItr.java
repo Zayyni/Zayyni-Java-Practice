0c38cec6b18bd3e02b6e69d91bc68e620046671d
Zayyni <zainulislam.733@gmail.com>
2026-04-30 02:22:04 +0500

    we have the hasprevious method available as well to traverse from starting point to the last

--- Collection/src/LaunchItr.java
+++ Collection/src/LaunchItr.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.ListIterator;
 
 public class LaunchItr
 {
@@ -12,10 +13,22 @@ public class LaunchItr
         al.add(125);
         System.out.println(al);
 
-        Iterator itr= al.iterator(); //it is a cursor which is available at the doorstep of our collection
-        while (itr.hasNext()) // hasNext will check either data is available or not to avoid error
+//        Iterator itr = al.iterator(); //it is a cursor which is available at the doorstep of our collection
+//        while (itr.hasNext()) // hasNext will check either data is available or not to avoid error
+//        {
+//            System.out.println(itr.next()); // then next will get the data
+//        }
+
+//        ListIterator litr = al.listIterator();
+//        while (litr.hasNext()) // it will check the next
+//        {
+//            System.out.println(litr.next());
+//        }
+
+        ListIterator litr = al.listIterator(al.size());
+        while (litr.hasPrevious()) //it will check the previous
         {
-            System.out.println(itr.next()); // then next will get the data
+            System.out.println(litr.previous());
         }
     }
 }
