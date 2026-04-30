275d749f606ca24ddb6d11255e1c785510cc3065
Zayyni <zainulislam.733@gmail.com>
2026-05-01 01:09:42 +0500

    in arrays we have type safety but in Collections we do not have typeSafety

---
+++ Collection/src/LaunchGen1.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class LaunchGen1
+{
+    public static void main(String[] args) {
+        // typeSafety
+        int arr[] = new int[3];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+
+        int data = arr[0];
+        System.out.println(data);
+
+        //typeSafety
+        String []str = new String[3];
+        str[0] = "Hello";
+        str[1] = "World";
+        str[2] = "Java";
+        System.out.println(str[0]);
+
+        //no typeSafety
+        ArrayList list = new ArrayList();
+        list.add("Hello");
+        list.add("World");
+        list.add(11);
+        list.add(26.5);
+
+        String info = (String) list.get(2);
+        System.out.println(info);   // ClassCastException
+
+
+    }
+}
