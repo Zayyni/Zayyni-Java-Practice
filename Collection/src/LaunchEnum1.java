ba7f33883db0807acdf9fcf1e9d74cb7b67506eb
Zayyni <zainulislam.733@gmail.com>
2026-05-06 01:45:50 +0500

    initial commit of enum

--- Collection/src/LaunchEnum1.java
+++ Collection/src/LaunchEnum1.java
@@ -1,6 +1,15 @@
+enum Week{
+    MON,TUE,WED,THU,FRI,SAT,SUN;
+}
+
+
 public class LaunchEnum1 {
-    public static void main(String[] args) {
-        System.out.println("Initial Commit of Enum");
-        
+    public static void main(String[] args)
+    {
+        Week week=Week.MON;
+        System.out.println(week);
+        int index = Week.THU.ordinal(); // it gives the index of the element and starts index from zero
+        System.out.println(index);
+
     }
 }
