ce2a7ed894f9ab1c1540d4bf4e86617416bfc1b1
Zayyni <zainulislam.733@gmail.com>
2026-05-09 02:34:28 +0500

    program to create inter connection between threads

---
+++ Multithreading/src/LaunchInterThread1.java
@@ -0,0 +1,82 @@
+class Producer extends Thread
+{
+    Queue q;
+    int i =1;
+    public Producer(Queue q)
+    {
+        this.q = q;
+    }
+    @Override
+    public void run()
+    {
+        while(true)
+        {
+            q.produce(i++);
+        }
+    }
+}
+class Consumer extends Thread
+{
+Queue q;
+public Consumer(Queue q)
+{
+    this.q = q;
+
+}
+    @Override
+    public void run()
+    {
+        while(true)
+        {
+            q.consume();
+        }
+    }
+
+}
+class Queue
+{
+    int data;
+    boolean flag = false;
+    synchronized public void produce(int i) {
+        try {
+            if (flag == true) {
+                wait();
+            }
+            else {
+                data = i;
+                System.out.println("Producer " + i + " produced " + data);
+                flag = true;
+                notify();
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    synchronized public void consume()
+    {
+        try {
+            if (flag == false) {
+                wait();
+            }
+            else {
+                System.out.println("Consumer " + data + " consumed");
+                flag = false;
+                notify();
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
+public class LaunchInterThread1
+{
+    public static void main(String[] args) {
+        Queue q = new Queue();
+        Producer p = new Producer(q);
+        Consumer c = new Consumer(q);
+        p.start();
+        c.start();
+    }
+}
