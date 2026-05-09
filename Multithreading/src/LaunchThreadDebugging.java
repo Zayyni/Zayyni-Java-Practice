e6698382221d62df435e99652c69fca4fe2ea2f0
Zayyni <zainulislam.733@gmail.com>
2026-05-10 01:19:29 +0500

    another method to rename the thread

--- Multithreading/src/LaunchThreadDebugging.java
+++ Multithreading/src/LaunchThreadDebugging.java
@@ -1,13 +1,26 @@
 
 public class LaunchThreadDebugging extends Thread
 {
+    public LaunchThreadDebugging()
+    {
+
+    }
+    public LaunchThreadDebugging(String name)
+    {
+        super(name);
+    }
     public static void main(String[] args) {
 
         System.out.println("LaunchThreadDebugging started");
-        LaunchThreadDebugging thread = new LaunchThreadDebugging();
-        LaunchThreadDebugging thread2 = new LaunchThreadDebugging();
-        thread.setName("MyThread 1");
-        thread2.setName("MyThread 2");
+//        LaunchThreadDebugging thread = new LaunchThreadDebugging();
+//        LaunchThreadDebugging thread2 = new LaunchThreadDebugging();
+//        thread.setName("MyThread 1");
+//        thread2.setName("MyThread 2");
+
+        LaunchThreadDebugging thread = new LaunchThreadDebugging("MyThread1");
+        LaunchThreadDebugging thread2 = new LaunchThreadDebugging("MyThread2");
+
+
         thread.start();
         thread2.start();
     }
