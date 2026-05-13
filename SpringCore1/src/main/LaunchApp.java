36ba3a1b7d96b58338ec9050fc2874f6bb90ebee
Zayyni <zainulislam.733@gmail.com>
2026-05-14 03:03:59 +0500

    commenting out the tight coupled code

--- SpringCore1/src/main/LaunchApp.java
+++ SpringCore1/src/main/LaunchApp.java
@@ -1,21 +1,21 @@
 package main;
-
-class Alpha{
-
-    public void alpha(){
-        //statements
-        //logic
-
-    }
-
-}
-class Beta{
-
-    public void beta(){
-        Alpha beta = new Alpha();
-        beta.alpha();
-    }
-}
+//
+//class Alpha{
+//
+//    public void alpha(){
+//        //statements
+//        //logic
+//
+//    }
+//
+//}
+//class Beta{
+//
+//    public void beta(){
+//        Alpha beta = new Alpha();
+//        beta.alpha();
+//    }
+//}
 
 public class LaunchApp {
     public static void main(String[] args) {
