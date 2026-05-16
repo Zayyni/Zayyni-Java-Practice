9e2b4ec24f647bc2853364bffdb739b590e64aae
Zayyni <zainulislam.733@gmail.com>
2026-05-16 17:03:00 +0500

    implemented Beanfactory Ioc

--- SpringCore3/src/main/java/main/LaunchMain2.java
+++ SpringCore3/src/main/java/main/LaunchMain2.java
@@ -1,21 +1,27 @@
 package main;
 
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
 
-public class LaunchMain {
+public class LaunchMain2 {
     public static void main(String[] args) {
 
         //Two Types of bean IOC container
         //1. BeanFactory
         //2. ApplicationContext
-        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        Zayyni z = context.getBean(Zayyni.class);
+        DefaultListableBeanFactory beanFactory=new DefaultListableBeanFactory();
+        XmlBeanDefinitionReader reader=new XmlBeanDefinitionReader(beanFactory);
+        reader.loadBeanDefinitions(new ClassPathResource("applicationContext.xml"));
+        Zayyni z = beanFactory.getBean(Zayyni.class);
         Boolean status= z.buyTheCourse(888.5);
         if (status) {
             System.out.println("The course has been purchased");
         }else {
             System.out.println("The course has not been purchased");
         }
+
     }
 }
