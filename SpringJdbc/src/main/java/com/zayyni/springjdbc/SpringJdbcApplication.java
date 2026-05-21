0d566d445b0c8250565fe9513466d6c3a728e417
Zayyni <zainulislam.733@gmail.com>
2026-05-22 02:58:27 +0500

    used datasource to connect with hikariCP

--- SpringJdbc/src/main/java/com/zayyni/springjdbc/SpringJdbcApplication.java
+++ SpringJdbc/src/main/java/com/zayyni/springjdbc/SpringJdbcApplication.java
@@ -1,13 +1,27 @@
 package com.zayyni.springjdbc;
 
+import com.zayyni.springjdbc.dao.EmployeeDaoImpl;
+import com.zayyni.springjdbc.model.Employee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
 
 @SpringBootApplication
 public class SpringJdbcApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringJdbcApplication.class, args);
+       ApplicationContext container=SpringApplication.run(SpringJdbcApplication.class, args);
+       EmployeeDaoImpl dao = container.getBean(EmployeeDaoImpl.class);
+
+
+//       List<Employee> list =dao.getEmployeesInfo();
+//       for (Employee employee : list) {
+//           System.out.println(employee);
+//       }
+
+        dao.getEmployeesInfo().forEach(System.out::println);
     }
 
 }
