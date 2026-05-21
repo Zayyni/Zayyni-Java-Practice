ada9e9b25e55c8d5360befc9633c0f60d1af7cff
Zayyni <zainulislam.733@gmail.com>
2026-05-22 02:38:01 +0500

    implemented pojo class for spring jdbc

--- SpringJdbc/src/main/java/com/zayyni/springjdbc/dao/EmployeeDaoImpl.java
+++ SpringJdbc/src/main/java/com/zayyni/springjdbc/dao/EmployeeDaoImpl.java
@@ -1,13 +1,21 @@
 package com.zayyni.springjdbc.dao;
 
 import com.zayyni.springjdbc.model.Employee;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository("dao")
 public class EmployeeDaoImpl implements IEmpoyeeDao{
 
     @Override
     public List<Employee> getEmployeesInfo() {
+        try {
+            //register driver
+            //connection
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
         return List.of();
     }
 }
