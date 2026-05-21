d5df45e842f6c0340720ae85fd0101ee9002db48
Zayyni <zainulislam.733@gmail.com>
2026-05-22 02:54:54 +0500

    used datasource to connect with hikariCP

--- SpringJdbc/src/main/java/com/zayyni/springjdbc/dao/EmployeeDaoImpl.java
+++ SpringJdbc/src/main/java/com/zayyni/springjdbc/dao/EmployeeDaoImpl.java
@@ -6,19 +6,41 @@ import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 @Repository("dao")
 public class EmployeeDaoImpl implements IEmpoyeeDao{
 
     @Autowired
     private DataSource dataSource;
+    private String SQL_QUERY = "select * from employees";
+
+    List<Employee> employees= null;
     @Override
     public List<Employee> getEmployeesInfo() {
         try {
             //register driver
             //connection
             Connection connection =dataSource.getConnection();
-            
+            PreparedStatement pstmnt = connection.prepareStatement(SQL_QUERY);
+            ResultSet resultSet =pstmnt.executeQuery();
+            employees = new ArrayList<>();
+
+            while(resultSet.next()){
+                Employee employee = new Employee();
+//                Integer i = resultSet.getInt(1);
+//                employee.setId(i);
+//
+//                employee.setName(resultSet.getString(2));
+//
+                employee.setId(resultSet.getInt(1));
+                employee.setName(resultSet.getString(2));
+                employee.setCity(resultSet.getString(3));
+                employees.add(employee);
+            }
+
 
         }
         catch (Exception e) {
