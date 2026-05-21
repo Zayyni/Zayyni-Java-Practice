a9a1551b179218d34739c12576aa1beb76c348f7
Zayyni <zainulislam.733@gmail.com>
2026-05-22 02:24:53 +0500

    initial commit of spring + jdbc app

---
+++ SpringJdbc/src/test/java/com/zayyni/springjdbc/SpringJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zayyni.springjdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
