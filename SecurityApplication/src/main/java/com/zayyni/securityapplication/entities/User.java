ff38db989013d19f5a7d488f854e64b786873b4e
Zayyni <zainulislam.733@gmail.com>
2026-08-20 02:02:15 +0500

    implemented security config filters on the bases of different use cases

--- SecurityApplication/src/main/java/com/zayyni/securityapplication/entities/User.java
+++ SecurityApplication/src/main/java/com/zayyni/securityapplication/entities/User.java
@@ -1,4 +1,35 @@
 package com.zayyni.securityapplication.entities;
 
-public class User {
+import jakarta.persistence.*;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
 }
--- SecurityApplication/src/main/java/com/zayyni/securityapplication/services/UserService.java
+++ SecurityApplication/src/main/java/com/zayyni/securityapplication/services/UserService.java
@@ -1,4 +1,22 @@
 package com.zayyni.securityapplication.services;
 
-public class UserService {
+import com.zayyni.securityapplication.exceptions.ResourceNotFoundException;
+import com.zayyni.securityapplication.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+//@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> new ResourceNotFoundException("User with email " + username + " not found"));
+    }
 }
