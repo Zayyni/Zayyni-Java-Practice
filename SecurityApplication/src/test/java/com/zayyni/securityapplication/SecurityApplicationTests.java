9863d4b5a0daae871258bbb72db66a6309a30c7d
Zayyni <zainulislam.733@gmail.com>
2026-08-20 22:56:58 +0500

    implemented jwt token service to create the tokens

--- SecurityApplication/src/test/java/com/zayyni/securityapplication/SecurityApplicationTests.java
+++ SecurityApplication/src/test/java/com/zayyni/securityapplication/SecurityApplicationTests.java
@@ -1,13 +1,28 @@
 package com.zayyni.securityapplication;
 
+import com.zayyni.securityapplication.entities.User;
+import com.zayyni.securityapplication.services.JwtService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SecurityApplicationTests {
 
+    @Autowired
+    private JwtService jwtService;
+
     @Test
     void contextLoads() {
+
+        User user = new User(4L,"zayyni@gmail.com","1234");
+
+        String token = jwtService.generateJwtToken(user);
+        System.out.println(token);
+
+        Long userId = jwtService.getUserIdFromToken(token);
+        System.out.println(userId);
+
     }
 
 }
